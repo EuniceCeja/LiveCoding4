07b90b243c599d892ef0404aa4cc8f8e732c5759
EuniceCeja <eceja93@hotmail.com>
2024-01-26 15:35:43 -0600

    Practica Live coding4

---
+++ src/org/generation/liveCoding4.java
@@ -0,0 +1,23 @@
+package org.generation;
+
+public class liveCoding4 {
+	 public static void imprimir() {
+	        for (int i = 1; i <= 100; i++) {
+	            if (i % 3 == 0 && i % 5 == 0) {
+	                System.out.println("FizzBuzz");
+	            } else if (i % 3 == 0) {
+	                System.out.println("Fizz");
+	            } else if (i % 5 == 0) {
+	                System.out.println("Buzz");
+	            } else {
+	                System.out.println(i);
+	            }
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        liveCoding4 fizzBuzz = new liveCoding4();
+	        liveCoding4.imprimir();
+	    
+	    }//main
+}//class
